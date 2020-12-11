c85ce0caa1d411612e289036a03ef768ab795e46
Romano Livi <romano_livi@outlook.com>
2020-12-11 20:11:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  
+  new_array = []
+  array.each do |i|
+    new_array << i ** 2
+  end
+  
+  return new_array
 end
+
+
+
+
+inspect.square_array([1,2,3])
+
+
+
